10e76f6b1224d393ff33802da078482a2bf18985
Celso <celsobenedetti2@gmail.com>
2022-08-02 19:52:20 -0300

    [UPDATE] changed jwt guard to ref handler metadata instead of class

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,20 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AllowUnauthorizedRequest } from 'src/common/decorators';
-import { AuthService } from './auth.service';
-import { SignInDto } from './dto/signin.dto';
-import { SignUpDto } from './dto/signup.dto';
+import { Body, Controller, Post } from "@nestjs/common";
+import { AllowUnauthorizedRequest } from "src/common/decorators";
+import { AuthService } from "./auth.service";
+import { SignInDto } from "./dto/signin.dto";
+import { SignUpDto } from "./dto/signup.dto";
 
-@AllowUnauthorizedRequest()
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('signup')
+  @Post("signup")
+  @AllowUnauthorizedRequest()
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
 
-  @Post('signin')
+  @Post("signin")
+  @AllowUnauthorizedRequest()
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
