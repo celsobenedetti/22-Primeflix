8cfdea33fd1449ce8354ac6300a27bae509d1864
Celso <celsobenedetti2@gmail.com>
2022-08-04 09:20:22 -0300

    [NEW] footer layout

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,31 +1,13 @@
 <script setup>
-import { ref } from "vue";
-
-let counter = ref(0);
-let myState = ref(0);
-
-setInterval(() => {
-  counter.value++;
-}, 1000);
+import FooterBar from "./components/FooterBar.vue";
 </script>
 
 <template>
   <div>
-    <header class="shadow  bg-main-900" v-if="$route.meta.title">
-      <div class="py-6 px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1 @click="counter = 0" class="text-3xl font-bold leading-tight text-gray-900">
-          {{ $route.meta.title }} / {{ counter }}
-        </h1>
-      </div>
-
-      <div class="py-6 px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1 @click="myState++" class="text-3xl font-bold leading-tight text-gray-900">
-          {{ myState }}
-        </h1>
-      </div>
-    </header>
+    <header class="shadow" v-if="$route.meta.title"></header>
     <main>
       <router-view />
     </main>
+    <FooterBar>Johnson</FooterBar>
   </div>
 </template>
