8d01fd2c14405c551d40cc8e42276abed8d63f0e
Celso <celsobenedetti2@gmail.com>
2022-08-05 13:15:28 -0300

    [NEW] vuex store setup

---
+++ web/src/store/modules/loading.ts
@@ -0,0 +1,19 @@
+import { LoadingState, StoreCommit } from "../../interfaces/vuex";
+
+export default {
+  state: () => ({ isLoading: false }),
+
+  actions: {
+    initLoading({ commit }: StoreCommit) {
+      commit("setLoading", true);
+    },
+    resolveLoading({ commit }: StoreCommit) {
+      commit("setLoading", false);
+    },
+  },
+  mutations: {
+    setLoading(state: LoadingState, payload: boolean) {
+      state.isLoading = payload;
+    },
+  },
+};
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,12 @@
-<script setup></script>
+<script setup lang="ts">
+import Logo from "@/assets/logo.png";
+</script>
 
 <template>
-  <div class="bg-main-900"></div>
+  <div class="bg-main-900">
+    <div class="flex gap-1 justify-center items-center p-2 w-full text-xl">
+      <p class="text-light">Primeflix</p>
+      <img :src="Logo" class="w-8 h-8" alt="" />
+    </div>
+  </div>
 </template>
