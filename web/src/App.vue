b8cb1cc4f30e6808d81c461703abf9789efed880
Celso <celsobenedetti2@gmail.com>
2022-08-05 21:03:50 -0300

    [UPDATE] vite typescript configs

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,9 +3,8 @@ import FooterBar from "./components/FooterBar.vue";
 </script>
 
 <template>
-  <!-- <header class="shadow" v-if="$route.meta.title"></header> -->
   <main class="pb-16 h-screen">
     <router-view />
   </main>
-  <FooterBar>Johnson</FooterBar>
+  <FooterBar />
 </template>
