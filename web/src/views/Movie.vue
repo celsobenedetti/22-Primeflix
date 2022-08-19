3019adf3e93a7302dbe7f2e34dd231094f5378a6
Celso Patiri <celsobenedetti2@gmail.com>
2022-08-19 13:40:00 -0300

    [NEW] watchlist screen and bookmark features

--- web/src/views/Movie.vue
+++ web/src/views/Movie.vue
@@ -1,38 +1,74 @@
 <script setup lang="ts">
-import { BookmarkIcon as BookmarkIconOutline, ArrowCircleLeftIcon } from "@heroicons/vue/outline";
+import {
+  BookmarkIcon as BookmarkIconOutline,
+  ArrowCircleLeftIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/vue/outline";
+import { BookmarkIcon as BookmarkIconSolid } from "@heroicons/vue/solid";
 import { StarIcon } from "@heroicons/vue/solid";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 import LoadingScreen from "@/components/LoadingScreen.vue";
 import FooterBar from "@/components/FooterBar.vue";
+import ModalAlert from "@/components/ModalAlert.vue";
 
-import { useGetTMDB } from "../api";
+import { postToggleBookmarkAsync, useGetTMDB } from "../api";
 import { useStore } from "../store";
-import { computed } from "@vue/reactivity";
+import { computed, ref } from "@vue/reactivity";
 
 const route = useRoute();
+const router = useRouter();
 
 const store = useStore();
+store.dispatch("fetchBookmarks");
+
+const sessionToken = store.getters.sessionToken;
 const posterBaseUrl = store.getters.posterUrlTMDB;
 const backdropBaseUrl = store.getters.backdropUrlTMDB;
 
-const { isLoading, data } = useGetTMDB(`movie/${route.params.id}`);
+const tmdbId = route.params.id;
+
+const { isLoading, data } = useGetTMDB(`movie/${tmdbId}`);
 
 const movieInfo = computed(() => {
   return {
-    posterUrl: `${posterBaseUrl}${data.value.poster_path}`,
-    backdropUrl: `${backdropBaseUrl}${data.value.backdrop_path}`,
-    title: data.value.title,
+    posterUrl: `${posterBaseUrl}${data.value?.poster_path}`,
+    backdropUrl: `${backdropBaseUrl}${data.value?.backdrop_path}`,
+    title: data.value?.title,
     voteAverage: data.value?.vote_average.toFixed(1),
     genres: data.value?.genres?.map(({ name }: { name: string }) => name),
     overview: data.value?.overview,
     releaseDate: data.value?.release_date,
+    posterPath: data.value?.poster_path,
+    genreIds: data.value?.genres,
     status: data.value?.status,
     rateCount: data.value?.vote_count,
     duration: `${data.value?.runtime} minutes`,
     tagline: data.value?.tagline,
   };
 });
+
+const bookmarks = computed(() => store.getters.bookmarks);
+
+const isBookmarked = computed(() => {
+  const wasBookmarked = bookmarks.value?.some(({ id_tmdb }: any) => +tmdbId === id_tmdb);
+  return (wasBookmarked || response.value?.status === 201) && response.value?.status !== 204;
+});
+
+const { isLoading: isBookmarkLoading, execute, response } = postToggleBookmarkAsync(sessionToken);
+
+const showSignInModal = ref(false);
+
+const clickBookmark = () => {
+  if (!sessionToken) return (showSignInModal.value = true);
+  if (isBookmarkLoading.value) return;
+
+  execute({
+    ...movieInfo.value,
+    genreIds: movieInfo.value?.genreIds?.map((genre: any) => genre.id),
+    id_tmdb: +tmdbId,
+  });
+};
 </script>
 
 <template>
@@ -41,7 +77,19 @@ const movieInfo = computed(() => {
     <header class="flex justify-between py-4 px-4 w-full text-light citems-center">
       <ArrowCircleLeftIcon @click="$router.go(-1)" class="w-7" />
       <h1 class="font-bold">Detail</h1>
-      <BookmarkIconOutline class="w-8" />
+      <DotsHorizontalIcon class="w-8" v-if="isBookmarkLoading" />
+      <template v-else>
+        <BookmarkIconSolid
+          v-if="isBookmarked"
+          @click="clickBookmark"
+          class="w-8 cursor-pointer text-secondary"
+        />
+        <BookmarkIconOutline
+          v-else
+          @click="clickBookmark"
+          class="w-8 cursor-pointer text-inactive"
+        />
+      </template>
     </header>
     <div class="relative">
       <img
@@ -110,4 +158,13 @@ const movieInfo = computed(() => {
     </main>
   </div>
   <FooterBar />
+
+  <ModalAlert
+    v-if="showSignInModal"
+    title="Login required"
+    content="You must be logged in to set Bookmarks"
+    buttonText="Login"
+    @close-modal-button="router.push('/signin')"
+    @close-modal-x="showSignInModal = false"
+  />
 </template>
